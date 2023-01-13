a78e8076ed497e0dc067025757a09322d5a81930
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-01-13 13:36:08 +0800

    onclick feature

---
+++ week5/javascript.js
@@ -0,0 +1,9 @@
+function changeText1(id) {
+    id.innerHTML = "HTML (Visited)";
+}
+function changeText2(id) {
+    id.innerHTML = "CSS (Visited)";
+}
+function changeText3(id) {
+    id.innerHTML = "JavaScript (Visited)";
+}
