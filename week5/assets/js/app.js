bd51a913a17779d7357ae61f0aba7d8135195ec1
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-01-22 22:00:02 +0800

    Put custom script in seperate file and added indicators

---
+++ week5/assets/js/app.js
@@ -0,0 +1,27 @@
+//Typing effect
+var type = new Typed (".auto-type",{
+    strings: ["CODER", "STUDENT", "GAMER", "CYCLIST", "WEIGHT LIFTER"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+    })
+//Mute and Unmute Button for interest video
+var vid = document.getElementById("interest-vid");
+
+        function mute() { 
+            vid.muted = true;
+        }
+        function umute(){
+            vid.muted = false
+        }
+
+        function clicked(){
+            if (vid.muted === false){
+                mute();
+                document.getElementById("unmute-button").innerHTML = "Unmute Video";
+            }else{
+                umute();
+                document.getElementById("unmute-button").innerHTML = "Mute Video";
+            }
+            
+        }
