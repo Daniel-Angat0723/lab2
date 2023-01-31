2ac9a5e924dc2fdf2afd2342774d181d21d67bc0
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-01-31 15:16:24 +0800

    Adding initial form

--- week9/index.php
+++ week9/index.php
@@ -457,8 +457,8 @@
         </section>
         <!-- //RESOURCES -->
 
-        <!-- RESOURCES -->
-        <section id="contactme" class="full-height px-lg-5">
+        <!-- FORM -->
+        <section id="contact" class="full-height px-lg-5">
             <div class="container">
 
                 <div class="row pb-4" data-aos="fade-up">
@@ -468,10 +468,60 @@
                     </div>
                 </div>
 
+                <?php
+                // define variables and set to empty values
+                $name = $email = $gender = $comment = $website = "";
+
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $name = test_input($_POST["name"]);
+                $email = test_input($_POST["email"]);
+                $website = test_input($_POST["website"]);
+                $comment = test_input($_POST["comment"]);
+                $gender = test_input($_POST["gender"]);
+                }
+
+                function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+                }
+                ?>
+
+                <h2>PHP Form Validation Example</h2>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+                Name: <input type="text" name="name">
+                <br><br>
+                E-mail: <input type="text" name="email">
+                <br><br>
+                Website: <input type="text" name="website">
+                <br><br>
+                Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+                <br><br>
+                Gender:
+                <input type="radio" name="gender" value="female">Female
+                <input type="radio" name="gender" value="male">Male
+                <input type="radio" name="gender" value="other">Other
+                <br><br>
+                <input type="submit" name="submit" value="Submit">  
+                </form>
+
+                <?php
+                echo "<h2>Your Input:</h2>";
+                echo $name;
+                echo "<br>";
+                echo $email;
+                echo "<br>";
+                echo $website;
+                echo "<br>";
+                echo $comment;
+                echo "<br>";
+                echo $gender;
+                ?>
 
             </div>
         </section>
-        <!-- //Form -->
+        <!-- //FORM -->
 
         <!-- FOOTER -->
         <footer class="py-5 px-lg-5 custom-footer">
