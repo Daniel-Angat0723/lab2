ed4f014bc49d35a2ce0e28a26e4b7fc108ce2414
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-02-08 22:40:26 +0800

    Added guests.php

---
+++ week9/guests.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "webprogss211";
+$password = "webprogss211";
+$dbname = "webprogss211";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, passowrd, email, website, comment, gender, reg_date FROM ddangat_myguests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email " . $row["email"]. " - Website " . $row["website"]. " - Comment " . $row["comment"]. " - Gender " . $row["gender"]. " - Date Registered " . $row["reg_date"]."<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
