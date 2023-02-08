0847c00f2c33fc0777065b61f38d8446fd3467a4
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-02-08 22:46:13 +0800

    Final fix

--- week9/guests.php
+++ week9/guests.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email " . $row["email"]. " - Website " . $row["website"]. " - Comment " . $row["comment"]. " - Gender " . $row["gender"]. " - Date Registered " . $row["reg_date"]."<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Encrypted Password: " . $row["password"]. " - Email " . $row["email"]. " - Website " . $row["website"]. " - Comment " . $row["comment"]. " - Gender " . $row["gender"]. " - Date Registered " . $row["reg_date"]."<br>";
   }
 } else {
   echo "0 results";
