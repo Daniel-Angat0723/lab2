c051c89b4b4c889f43b7c22c5d23e74d7e577e53
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-02-08 22:44:52 +0800

    guests.php bug fix

--- week9/guests.php
+++ week9/guests.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, passowrd, email, website, comment, gender, reg_date FROM ddangat_myguests";
+$sql = "SELECT id, name, password, email, website, comment, gender, reg_date FROM ddangat_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
