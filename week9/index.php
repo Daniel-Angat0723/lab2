b1c0b691a3c32d4194d5b709af1b06cd75efe87f
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-02-08 23:02:23 +0800

    Added input checker for now in index.php

--- week9/index.php
+++ week9/index.php
@@ -534,6 +534,20 @@
                 $data = stripslashes($data);
                 $data = htmlspecialchars($data);
                 return $data;
+
+                echo "<h2>Your Input:</h2>";
+                echo $name;
+                echo "<br>";
+                echo $encrypted_pass;
+                echo "<br>";
+                echo $email;
+                echo "<br>";
+                echo $website;
+                echo "<br>";
+                echo $comment;
+                echo "<br>";
+                echo $gender;
+                echo "<br>";
                 }
                 ?>
 
